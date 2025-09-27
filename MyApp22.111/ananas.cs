082e15b4428aa2d7febb6fcdc4766f01a4d3ab05
Javad Teymouri <teymouri.j7274@gmail.com>
2025-09-27 14:12:45 +0330

    add havij01.cs

---
+++ MyApp22.111/ananas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp22._111
+{
+    internal class ananas
+    {
+    }
+}
